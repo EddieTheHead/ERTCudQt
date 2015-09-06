3a928f3e0401692508f6f1c793715fcc928ec700
EddieTheHead <hubert.bossy@wp.pl>
2015-09-06 09:53:50 +0200

    poprawki na ostatnią chwilę

--- gpsdevice.cpp
+++ gpsdevice.cpp
@@ -11,7 +11,12 @@
 #include <MapAdapterWMS.h>
 #include <QMapControl.h>
 #include <memory>
-
+#include <QLayout>
+#include <QApplication>
+#include <QLabel>
+#include <GeometryWidget.h>
+#include <chrono>
+#include <degminsec.h>
 
 using namespace qmapcontrol;
 
@@ -19,53 +24,53 @@ using namespace qmapcontrol;
 GpsDevice::GpsDevice(QWidget *parent) :
     QMapControl(parent)
 {
-    //nowy obiekt widgetu map
-    //QSize mapSize(1024,720);
-    //this->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
-    //this->setMinimumSize(mapSize);
 
-    this->enablePersistentCache();
+    this->enablePersistentCache(std::chrono::minutes(0),QDir::currentPath() + QDir::separator() + "QMapControl.cache" );
+
+    this->setZoomMaximum(19);
+    this->setZoom(19);
     createLayers();
 
-    this->setZoomMaximum(17);
-    this->setZoom(17);
-
-    //odświeżanie
+//    odświeżanie
     RefresDelay = 500;
     DrawTimer.setSingleShot(false);
     DrawTimer.setInterval(RefresDelay);
     DrawTimer.start();
-  //  QObject::connect(&DrawTimer,SIGNAL(timeout()),this,SLOT(drawMap()));
+    QObject::connect(&DrawTimer,SIGNAL(timeout()),this,SLOT(drawPath()));
 
     //spójrz na Polibudę
-    PointWorldCoord PUT(16.950932,52.402205);
-    ERC = std::make_shared<PointWorldCoord>(PointWorldCoord(20.452138, 50.790648));
-    this->setMapFocusPoint(PUT);
+    PointWorldCoord ERC(20.461741,50.782566);
+    this->setMapFocusPoint(ERC);
     this->enableScalebar(true);
     //testowe
     pointerImage = QImage("://Pointer.png");
-    followingRower = true;
+    followingRower = false;
+    connect(this,SIGNAL(mouseEventPressCoordinate(QMouseEvent*,PointWorldCoord)),SLOT(onMousePressEvent(QMouseEvent*,PointWorldCoord)));
+    //connect(this,SIGNAL(geometryClicked(const Geometry*)),this,SLOT(onGeometryClicked(const Geometry*)));
+    PointSelectMode = false;
 
-    //setZoomMaximum(19);
+    setBackgroundColour(Qt::white);
 
-//    connect(map,SIGNAL(mouseEventPressCoordinate(QMouseEvent*,PointWorldCoord)),this,SLOT(displayCoursorCoords(QMouseEvent*,PointWorldCoord)));
+    readCheckpoints("://way.txt");
 
+//    addCheckPoint(PointWorldCoord(DegMinSec(20,27,43.29576).toDecimalDegress(),DegMinSec(50,46,57.15351).toDecimalDegress()),"1A");
+    addCheckPoint(PointWorldCoord(DegMinSec(20,27,41.75322).toDecimalDegress(),DegMinSec(50,46,57.46213).toDecimalDegress()),"1B");
+    addCheckPoint(PointWorldCoord(DegMinSec(20,27,40.88582).toDecimalDegress(),DegMinSec(50,46,56.34093).toDecimalDegress()),"2");
+    addCheckPoint(PointWorldCoord(DegMinSec(20,27,42.88572).toDecimalDegress(),DegMinSec(50,46,57.52350).toDecimalDegress()),"3");;
+    drawCheckpoints();
 }
 
 
 GpsDevice::~GpsDevice()
 {
-    DrawTimer.stop();
 }
 
-void GpsDevice::newGPSPosition(QPointF pos)
+void GpsDevice::newGPSPosition(PointWorldCoord pos)
 {
-    float latitude = pos.y();
-    float longitude = pos.x();
+    qDebug() << "lat = " << pos.latitude() << "long = " << pos.longitude();
+    auto freshPoint = std::make_shared<GeometryPoint>(pos);
 
-    qDebug() << "lat = " << latitude << "long = " << longitude;
-    auto freshPoint = std::make_shared<GeometryPoint>(longitude,latitude);
-    if (!CompetitionArea->touches(freshPoint.get(),this->getCurrentZoom()))
+    if (!OffsetArea->touches(freshPoint.get(),this->getCurrentZoom()))
     {
         qDebug() << "Koordynaty poza dozwolonym polem ";
         return;
@@ -73,13 +78,13 @@ void GpsDevice::newGPSPosition(QPointF pos)
     if(points.empty())
     {
         points.emplace_back(freshPoint);
-        this->setMapFocusPoint(PointWorldCoord(points.back()->coord()));
+        this->setMapFocusPoint(pos);
     }
-    else if(points.back()->coord().latitude() != latitude || points.back()->coord().longitude() != longitude)
+    else if(pos != points.back()->coord())
     {
         points.emplace_back(freshPoint);
     }
-    drawMap();
+    if(!points.empty() && followingRower) this->setMapFocusPointAnimated(pos);
 }
 
 void GpsDevice::newCompassAngle(float angle)
@@ -104,39 +109,55 @@ void GpsDevice::drawPath()
         pathLayer->addGeometry(point);
     }
     //rysowanie linii przechodzącej przez te punkty
-    std::shared_ptr<GeometryLineString> pathString(std::make_shared<GeometryLineString>(rawPoints));
+    auto pathString = std::make_shared<GeometryLineString>(rawPoints,0,19);
+//    if(points.size() < 2) return;
+//    std::vector<PointWorldCoord> twoLastPoints;
+//    twoLastPoints.push_back(points.at(points.size() - 2)->coord());
+//    twoLastPoints.push_back(points.back()->coord());
+//    auto pathString = std::make_shared<GeometryLineString>(twoLastPoints,0,19);
     QPen pen(Qt::red);
+    pen.setWidth(2);
     pathString->setPen(pen);
-    pen.setWidth(10);
-    if(!points.empty() && followingRower) this->setMapFocusPointAnimated(PointWorldCoord(points.back()->coord()));
     pathLayer->addGeometry(pathString);
 }
 
 bool GpsDevice::readCheckpoints(QString fileName)
 {
     QFile file(fileName);
-    if(!file.open(QFile::ReadOnly))
+    if(!file.open(QFile::ReadOnly | QFile::Text))
     {
+        qDebug() << "!file.open()";
         return 0;
     }
 
     QTextStream in(&file);
 
-    QPen dots_pen(QColor(0,255,0));
-        dots_pen.setWidth(3);
-
-    checkPointsList.clear();
+    //checkPointsList.clear();
 
     while(!in.atEnd())
     {
         QString line = in.readLine();
-        QStringList fields = line.split("&", QString::SkipEmptyParts);
 
-            checkPointsList.emplace_back(std::make_shared<GeometryPointCircle>(PointWorldCoord(fields[1].toFloat(),fields[2].toFloat() ), QSizeF(15.0, 15.0)));
-            checkPointsList.back()->setPen(dots_pen);
-            checkPointsList.back()->setMetadata(fields[0].toStdString(), "");//""Podzamcze 45, Checiny - ERC challenge"""");
+        QRegExp rx("(\\d{1,3})[°dh,\\s]{1,}(\\d{1,3})['m,\\s]{1,}(\\d{1,3}.\\d{1,})[s,'' ]{1,2}[ x,]{1,}(\\d{1,3})[°dh,\\s]{1,}(\\d{1,3})['m,\\s]{1,}(\\d{1,3}.\\d{1,})[s,']{1,2}");
+        rx.exactMatch(line);
+        QStringList captured = rx.capturedTexts();
+        qDebug() << "line" + line;
+        if(captured.size() == 7 )
+        {
+            qDebug() << "if(captured.size() == 7) true";
+            for(const auto &s: captured) qDebug() << s;
+            DegMinSec longitude(captured.at(1).toInt(),captured.at(2).toInt(),captured.at(3).toFloat());
+            DegMinSec latitude(captured.at(4).toInt(),captured.at(5).toInt(),captured.at(6).toFloat());
+            qDebug() << "z pliku";
+            qDebug() << longitude.toDecimalDegress();
+            qDebug() << latitude.toDecimalDegress();
+            PointWorldCoord point(longitude.toDecimalDegress(),latitude.toDecimalDegress());
+
+            addCheckPoint(point,"");
+        }
     }
     file.close();
+    drawCheckpoints();
     return true;
 }
 
@@ -144,22 +165,95 @@ void GpsDevice::createLayers()
 {
     std::set<projection::EPSG> projections;
     projections.insert(projection::EPSG::SphericalMercator);
-    this->addLayer(std::make_shared<LayerMapAdapter>("Warstwa z mapą OSM",
-                                                     std::make_shared<MapAdapterTile>(QUrl("http://tile.openstreetmap.org/%zoom/%x/%y.png"),
-                                                     projections, 0, 17, 0, false))); //testowa warstwa z własnym zoomem
+    mapLayer = std::make_shared<LayerMapAdapter>("Warstwa z mapą OSM",
+                                                 std::make_shared<MapAdapterTile>(QUrl(""),
+                                                 projections, 0, 19, 0, false));
+    this->addLayer(mapLayer); //testowa warstwa z własnym zoomem
+    this->addLayer(std::make_shared<LayerMapAdapter>("Warstwa z mapą Google", std::make_shared<MapAdapterGoogle>(MapAdapterGoogle::GoogleLayerType::SATELLITE))); //testowa warstwa googlwa
+    //obrazek z mapą
+    mapPixmapLayer = std::make_shared<LayerGeometry>("Map pixmap",0,19);
+    this->addLayer(mapPixmapLayer);
 
-
-    pointerLayer=std::make_shared<LayerGeometry>("Pointer layer");
+    checkPointslayer = std::make_shared<LayerGeometry>("Warstwa Checkpointów",0,19);
+    this->addLayer(checkPointslayer);
+    pointerLayer=std::make_shared<LayerGeometry>("Pointer layer",0,19);
     this->addLayer(pointerLayer);
-    //this->addLayer(std::make_shared<LayerMapAdapter>("Warstwa z mapą Google", std::make_shared<MapAdapterGoogle>())); //testowa warstwa googlwa
 
     //dodaję warstwę z przebiegiem trasy
-    pathLayer=std::make_shared<LayerGeometry>("Path layer");
+    pathLayer=std::make_shared<LayerGeometry>("Path layer",0,19);
     this->addLayer(pathLayer);
 
-    areaLayer = std::make_shared<LayerGeometry>("Warstwa z granicami");
+    areaLayer = std::make_shared<LayerGeometry>("Warstwa z granicami",0,19);
     this->addLayer(areaLayer);
+
+    infoLayer = std::make_shared<LayerGeometry>("Warstwa info",0,19);
+    this->addLayer(infoLayer);
 }
+
+void GpsDevice::calculateOffsetArea()
+{
+    std::vector<PointWorldCoord> polygonPoints = CompetitionArea->points();
+    std::vector<PointWorldCoord> offsetPoints;
+    const qreal offset = 0.0001;
+    offsetPoints.push_back(PointWorldCoord(polygonPoints[0].longitude() - offset,polygonPoints[0].latitude() + offset));
+    offsetPoints.push_back(PointWorldCoord(polygonPoints[1].longitude() + offset,polygonPoints[1].latitude() + offset));
+    offsetPoints.push_back(PointWorldCoord(polygonPoints[2].longitude() + offset,polygonPoints[2].latitude() - offset));
+    offsetPoints.push_back(PointWorldCoord(polygonPoints[3].longitude() - offset,polygonPoints[3].latitude() - offset));
+
+//    float eq1[2];
+//    calculateDiagonalEq(polygonPoints[0],polygonPoints[2],eq1);
+//    float eq2[2];
+//    calculateDiagonalEq(polygonPoints[1],polygonPoints[3],eq2);
+//    offsetPoints.push_back(calculaSingleOffsetPoint(polygonPoints[0],eq1,-offset));
+//    offsetPoints.push_back(calculaSingleOffsetPoint(polygonPoints[1],eq2,offset));
+//    offsetPoints.push_back(calculaSingleOffsetPoint(polygonPoints[2],eq1,offset));
+//    offsetPoints.push_back(calculaSingleOffsetPoint(polygonPoints[3],eq2,-offset));
+
+    OffsetArea = std::make_shared<GeometryPolygon>(offsetPoints,0,19);
+}
+
+void GpsDevice::addMapImage()
+{
+    mapPixmapLayer->clearGeometries();
+    auto mapGeometry = std::make_shared<GeometryPointImage>(PointWorldCoord(0,0),mapPixmap,0,19);
+    mapPixmapLayer->addGeometry(mapGeometry);
+}
+
+void GpsDevice::calculateDiagonalEq(PointWorldCoord left, PointWorldCoord right,float* data)
+{
+    float a = (left.longitude() - right.longitude())
+              / (left.latitude() - right.latitude());
+    float b = left.longitude() - left.latitude() * a;
+    data[0] = a;
+    data[1] = b;
+}
+
+PointWorldCoord GpsDevice::calculaSingleOffsetPoint(PointWorldCoord point, float *eq,float offset)
+{
+    float longitude = point.longitude() + offset;
+    float latitude = longitude * eq[0] + eq[1];
+    return PointWorldCoord(longitude,latitude);
+}
+
+
+
+void GpsDevice::chooseNewMapFile()
+{
+    QString filename = QFileDialog::getOpenFileName(this,"Choose new map image file",QApplication::applicationDirPath());
+    mapPixmap = std::make_shared<QPixmap>(QPixmap::fromImage(QImage(filename)));
+    if(mapPixmap->isNull()) return;
+    else addMapImage();
+}
+bool GpsDevice::getPointSelectMode() const
+{
+    return PointSelectMode;
+}
+
+void GpsDevice::setPointSelectMode(bool value)
+{
+    PointSelectMode = value;
+}
+
 std::shared_ptr<GeometryPolygon> GpsDevice::getCompetitionArea() const
 {
     return CompetitionArea;
@@ -183,7 +277,7 @@ void GpsDevice::setFollowingRower(bool value)
 
 void GpsDevice::drawDirection(float latitude,float longitude, float angle = 0)
 {
-    PointWorldCoord pointCoord(latitude,longitude);
+    PointWorldCoord pointCoord(longitude,latitude);
 
     if(pointerImage.isNull())
     {
@@ -196,37 +290,116 @@ void GpsDevice::drawDirection(float latitude,float longitude, float angle = 0)
     transMatrix.rotate(angle);
     QImage rotatedImage = pointerImage.transformed(transMatrix);
 
-    auto roverOrientation=std::make_shared<GeometryPointImage>(pointCoord, QPixmap::fromImage(rotatedImage));
+    auto roverOrientation=std::make_shared<GeometryPointImage>(pointCoord, QPixmap::fromImage(rotatedImage),0,19);
     pointerLayer->clearGeometries();
     pointerLayer->addGeometry(roverOrientation);
 }
 
 void GpsDevice::drawCheckpoints()
 {
-    //dodaję warstwę z punktami
-    std::shared_ptr<LayerGeometry> dots(std::make_shared<LayerGeometry>("Custom Layer"));
-    this->addLayer(dots);
-
+    if(checkPointsList.empty()) requestRedraw();
+    checkPointslayer->clearGeometries();
     for(const auto& point : checkPointsList)
     {
         // Also add the point to the custom layer.
-        dots->addGeometry(point);
+        checkPointslayer->addGeometry(point);
+
+        QLabel *infoLabel = new QLabel(this);
+        QString info =  point->metadata(("name")).toString();
+        qDebug() << "info wybranego punktu" << info;
+        infoLabel->setText(info);
+        infoLabel->setGeometry(0,0,100,100);
+        auto infoGeometry = std::make_shared<GeometryWidget>(point->coord(),infoLabel,0,19);
+        infoLayer->addGeometry(infoGeometry);
+        infoGeometry->setVisible(true);
+    }
+    if(checkPointsList.size() >= 3)
+    {
+        std::vector<PointWorldCoord> line;
+        line.push_back(PointWorldCoord(DegMinSec(20,27,40.88582).toDecimalDegress(),DegMinSec(50,46,56.34093).toDecimalDegress()));
+        line.push_back(PointWorldCoord(DegMinSec(20,27,42.88572).toDecimalDegress(),DegMinSec(50,46,57.52350).toDecimalDegress()));
+
+
+        auto lineString = std::make_shared<GeometryLineString>(line,0,19);
+        QPen pen(Qt::blue);
+        pen.setWidth(2);
+        lineString->setPen(pen);
+        checkPointslayer->addGeometry(lineString);
     }
 }
 
-void GpsDevice::drawMap()
+
+
+void GpsDevice::addCheckPoint(PointWorldCoord point, QString name = QString(""))
 {
-    drawPath();
+    QPen pen(QColor(0,255,0));
+    pen.setWidth(3);
+    checkPointsList.emplace_back(std::make_shared<GeometryPointCircle>(point, QSizeF(15.0, 15.0),0,19));
+    checkPointsList.back()->setPen(pen);
+    checkPointsList.back()->setMetadata(std::string("name"),name);
+    checkPointsList.back()->setMetadataDisplayed("name");
+    drawCheckpoints();
 }
+
+void GpsDevice::onMousePressEvent(QMouseEvent *e, PointWorldCoord coords)
+{
+    if(e->buttons() & Qt::MouseButton::LeftButton)
+    {
+        for(const auto &p : checkPointsList)
+        {
+            auto freshPoint = std::make_shared<GeometryPoint>(coords,0,19);
+            if(p->touches(freshPoint.get(),this->getCurrentZoom()))
+            {
+                QLabel *infoLabel = new QLabel(this);
+                QString info =  freshPoint->metadata(("name")).toString() + "\n";
+                qDebug() << "info wybranego punktu" << info;
+                infoLabel->setText(info);
+                infoLabel->setGeometry(0,0,200,100);
+                auto infoGeometry = std::make_shared<GeometryWidget>(p->coord(),infoLabel,0,19);
+                infoLayer->addGeometry(infoGeometry);
+                infoGeometry->setVisible(true);
+            }
+        }
+    }
+    else if(e->buttons() & Qt::MouseButton::RightButton && PointSelectMode)
+    {
+//        for(uint i =0; i < checkPointsList.size(); ++i)
+//        {
+//            auto freshPoint = std::make_shared<GeometryPoint>(coords);
+//            if(checkPointsList[i]->touches(freshPoint.get(),getCurrentZoom()))
+//            {
+//                checkPointsList.erase(checkPointsList.begin() + i);
+//            }
+//        }
+    }
+    drawCheckpoints();
+}
+
+//void GpsDevice::onGeometryClicked(const Geometry *geo)
+//{
+//    QMessageBox::information(this,"test",geo->metadata("name").toString(),QMessageBox::Ok);
+//}
 
 void GpsDevice::drawArea()
 {
     qDebug() << "Nowy obszar:";
     for(const auto &p:CompetitionArea->points()) qDebug() << "lon:" << QString("%1").arg(p.longitude(), 0, 'g', 5) << "lat:" << QString("%1").arg(p.latitude(), 0, 'g', 5);
-    QPen pen(Qt::red);
+    QPen pen(Qt::green);
     pen.setWidth(2);
     CompetitionArea->setPen(pen);
     areaLayer->clearGeometries();
     areaLayer->addGeometry(CompetitionArea);
+    calculateOffsetArea();
+    drawOffsetArea();
+}
+
+void GpsDevice::drawOffsetArea()
+{
+    QPen pen(Qt::red);
+    pen.setWidth(2);
+    QBrush brush(Qt::transparent);
+    OffsetArea->setBrush(brush);
+    OffsetArea->setPen(pen);
+    areaLayer->addGeometry(OffsetArea);
 }
 
--- gpsdevice.h
+++ gpsdevice.h
@@ -14,6 +14,7 @@
 #include <QWindow>
 #include <QMessageBox>
 #include <GeometryPolygon.h>
+#include <fillinfodialog.h>
 
 using namespace qmapcontrol;
 
@@ -29,37 +30,55 @@ public:
     bool getFollowingRower() const;
     std::shared_ptr<GeometryPolygon> getCompetitionArea() const;
 
+    bool getPointSelectMode() const;
+
+
 public slots:
     void setCompetitionArea(const std::shared_ptr<GeometryPolygon> &value);
     void setFollowingRower(bool value);
-    void newGPSPosition(QPointF pos);
+    void newGPSPosition(const PointWorldCoord pos);
     void newCompassAngle(float angle);
     void chooseNewCheckPointsFile();
     void drawArea();
-
+    void drawOffsetArea();
+    void setPointSelectMode(bool value);
+    void chooseNewMapFile();
 
 private slots:
     void drawPath();
     void drawDirection(float lat, float longitude, float angle);
     void drawCheckpoints();
-    void drawMap();
+    void addCheckPoint(PointWorldCoord point, QString name);
+    void onMousePressEvent(QMouseEvent* e, PointWorldCoord coords);
+    //void onGeometryClicked(const Geometry *geo);
 private:
     //void resizeEvent(QResizeEvent *event);
     bool readCheckpoints(QString fileName); // Funkcja czytująca punkty z pliku
     void createLayers();
+    void calculateOffsetArea();
+    void addMapImage();
+    void calculateDiagonalEq(PointWorldCoord left, PointWorldCoord right , float *data);
+    PointWorldCoord calculaSingleOffsetPoint(PointWorldCoord point, float *eq, float offset);
 
+    int RefresDelay = 500;
     QTimer DrawTimer;
-    int RefresDelay;
     std::shared_ptr<LayerGeometry> pathLayer;
     std::shared_ptr<LayerGeometry> pointerLayer;
     std::shared_ptr<LayerGeometry> areaLayer;
+    std::shared_ptr<LayerMapAdapter> mapLayer;
+    std::shared_ptr<LayerGeometry> mapPixmapLayer;
+    std::shared_ptr<LayerGeometry> infoLayer;
     std::vector<std::shared_ptr<GeometryPoint>> points;
     std::vector<std::shared_ptr<GeometryPoint>> checkPointsList;
     QImage pointerImage;
-    std::shared_ptr<GeometryPoint> currentPosition;
     bool followingRower;
     std::shared_ptr<PointWorldCoord> ERC;
     std::shared_ptr<GeometryPolygon> CompetitionArea;
+    std::shared_ptr<GeometryPolygon> OffsetArea;
+    std::shared_ptr<LayerGeometry> checkPointslayer;
+    bool PointSelectMode;
+    std::shared_ptr<QPixmap> mapPixmap;
+
 };
 
 #endif // GPSDEVICE_H
